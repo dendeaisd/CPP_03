e5d765fbc84c7a1083900ea8a49362718a385677
FlaviaV <flavia.voicuu@gmail.com>
2024-05-15 22:13:38 +0200

    added comments

--- ex01/ScavTrap.hpp
+++ ex01/ScavTrap.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ScavTrap.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fvoicu <fvoicu@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*   By: fvoicu <fvoicu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 22:59:07 by fvoicu            #+#    #+#             */
-/*   Updated: 2024/04/27 00:08:53 by fvoicu           ###   ########.fr       */
+/*   Updated: 2024/05/15 22:03:21 by fvoicu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,27 @@
 #include <iostream>
 #include "ClapTrap.hpp"
 
+/**
+ * @class ScavTrap
+ * @brief A derived class from ClapTrap designed 
+ * to model a more specialized character, 
+ * known as ScavTrap.
+ *
+ * ScavTrap inherits from ClapTrap and extends 
+ * it's functionality to include guarding gates 
+ * and specialized attacks. It is more specialized
+ * than a general ClapTrap and can be used in 
+ * scenarios where gate guarding and security 
+ * are required.
+ */
 class ScavTrap : public ClapTrap {
-
  public:
   ScavTrap();
   ScavTrap(const std::string& newName);
   ScavTrap(const ScavTrap& other);
   ScavTrap& operator=(const ScavTrap& other);
   ~ScavTrap();
+  
   void attack(const std::string& target);
   void guardGate();
 };
